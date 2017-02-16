97d07dcbef20d461ebc462621b1dd375d5f6ee1a
German Mikulski <mik.german@gmail.com>
2017-02-16 00:25:04 +0000

    Added content switching to the feature info box

--- wp-content/themes/landy/custom_js.js
+++ wp-content/themes/landy/custom_js.js
@@ -1,38 +1,50 @@
 
+/*CUSTOM JS EXTENDING LANDY THEME FUNCTIONALITY*/
+
 (function($) {
 	
 
 	$(document).ready(function() {
 
-
 		/*LEARN MORE SECTION*/
 
 		//is the learn more section opened?
 		var learn_opened = 0;
-		console.log('here is ', learn_opened);
-
 	    
 	    //when learn more is clicked
 		$('.learn').click(function(){
 
-	 		console.log('been clicked with ', learn_opened);
+			var source = $(this).attr('class').split(' ').pop();
+			console.log(source);
 
+	 		//if already opened
 	 		if(learn_opened == 1){
 
+	 			//close the placeholder
 	 			$('.more_info').css('height', '0');
 	 			learn_opened = 0;
+
+	 			//hide all content
+	 			$('.feature_content').css('display', 'none');
+
+
 	 		}
+	 		//if not opened yet
 	 		else {
 
+	 			//open the placeholder
 	 			$('.more_info').css('height', '300px');
 	 			learn_opened = 1;
+
+	 			//display an appropriate content piece
+	 			$('.feature_content.' + source).css('display', 'block');
+
 	 		}
 		});
 
 
-		// //set up svg logo
-		// $('logo animated fadeIn').children('a').children('img').addClass('style-svg');
-		// $('logo animated fadeIn').children('a').children('img').css('height', '40px');
+		/*TESTIMONIALS SECTION*/
+
 
 	});
 

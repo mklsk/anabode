f727183878b63f29b5f258bcc67fdeb0995b77ca
German Mikulski <mik.german@gmail.com>
2017-02-22 23:36:10 +0000

    Full functionality of features section

--- wp-content/themes/landy/custom_js.js
+++ wp-content/themes/landy/custom_js.js
@@ -1,89 +1,110 @@
-
 /*CUSTOM JS EXTENDING LANDY THEME FUNCTIONALITY*/
 
 (function($) {
-	
-
-	$(document).ready(function() {
-
-		/*LEARN MORE SECTION*/
-
-		//is the learn more section opened?
-		var learn_opened = 0;
-	    
-	    //when learn more is clicked
-		$('.learn').click(function(){
-
-			var source = $(this).attr('class').split(' ').pop();
-			console.log(source);
-
-	 		//if already opened
-	 		if(learn_opened == 1){
-
-	 			//close the placeholder
-	 			$('.more_info').css('height', '0');
-	 			//hide "close" button
-	 			$('.close_features').css('opacity', '0');
-
-	 			learn_opened = 0;
-
-	 			//hide all content
-	 			$('.feature_content').css('display', 'none');
 
 
-	 		}
-	 		//if not opened yet
-	 		else {
+    $(document).ready(function() {
 
-	 			//open the placeholder
-	 			$('.more_info').css('height', '300px');
-	 			//display "close" button
-	 			$('.close_features').css('opacity', '1');
+        /*LEARN MORE SECTION*/
 
-	 			learn_opened = 1;
+        var all_closed = true;
 
-	 			//display an appropriate content piece
-	 			$('.feature_content.' + source).css('display', 'block');
+        //when learn more is clicked
+        $('.learn').click(function() {
 
-	 		}
-		});
+        	console.log(all_closed);
 
+            source = $(this).attr('class').split(' ').pop();
 
-		/*TESTIMONIALS SECTION*/
+            if (all_closed) {
+
+                //open the placeholder
+                $('.more_info').css('height', '300px');
 
 
-		//add quotation marks to the quotes
-		$('.text-blockquote.custom').children('p').append('<span class="quote_mark">&ldquo;</span>');
-		$('.text-blockquote.custom').children('p').prepend('<span class="quote_mark">&rdquo;</span>');
+                setTimeout(function() {
 
-		//on section hover
-		$('.one_testim').hover(
-
-			//in callback
-			function(){
-
-			//dispaly filter
-			$(this).children('.filter').css('opacity', '0.7');
-			//show quote and hide title
-			$(this).children('.text-blockquote.custom').children('p').css('opacity', '1');
-			$(this).children('.text-blockquote.custom').children('h6').css('opacity', '0');
+                    //display "close" button
+                    $('.close_features').css('opacity', '1');
 
 
 
+                    //display an appropriate content piece
+                    $('.feature_content.' + source).show(300);
+                    $('.feature_content.' + source).attr('opened', 'true');
+                    $('.close_features').css('display', 'block');
 
-			//out callback
-		}, function(){
+                    all_closed = false;
 
-				//hide filter
-				$(this).children('.filter').css('opacity', '0');
-				//hide quote and show title
-				$(this).children('.text-blockquote.custom').children('p').css('opacity', '0');
-				$(this).children('.text-blockquote.custom').children('h6').css('opacity', '1');
-
-		});
+                }, 700);
 
 
-	});
+            } else {
+
+
+                $('.feature_content[opened]').hide(300, function() {
+
+                    //display an appropriate content piece
+                    $('.feature_content.' + source).show(300);
+                    $('.feature_content.' + source).attr('opened', 'true');
+
+                });
+            }
+
+        });
+
+        //when close button is clicked
+        $('.close_features').click(function() {
+
+
+            //close the placeholder
+            $('.more_info').css('height', '0');
+            //hide "close" button
+            $('.close_features').css('opacity', '0');
+            //hide all content
+            $('.feature_content').hide(300);
+            $('.feature_content.' + source + "[opened='true']").removeAttr('opened');
+            all_closed = true;
+
+        });
+
+
+        /*TESTIMONIALS SECTION*/
+
+
+        //add quotation marks to the quotes
+        $('.text-blockquote.custom').children('p').append('<span class="quote_mark">&ldquo;</span>');
+        $('.text-blockquote.custom').children('p').prepend('<span class="quote_mark">&rdquo;</span>');
+
+        //on section hover
+        $('.one_testim').hover(
+
+            //in callback
+            function() {
+
+                //dispaly filter
+                $(this).children('.filter').css('opacity', '0.7');
+                //show quote and hide title
+                $(this).children('.text-blockquote.custom').children('p').css('opacity', '1');
+                $(this).children('.text-blockquote.custom').children('h6').css('opacity', '0');
+
+
+
+
+                //out callback
+            },
+            function() {
+
+                //hide filter
+                $(this).children('.filter').css('opacity', '0');
+                //hide quote and show title
+                $(this).children('.text-blockquote.custom').children('p').css('opacity', '0');
+                $(this).children('.text-blockquote.custom').children('h6').css('opacity', '1');
+
+            });
+
+
+    });
 
 
 })(jQuery);
